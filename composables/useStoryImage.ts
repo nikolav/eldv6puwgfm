0681f1f13728d4f05eaa8562c9cf0c3599679dea
Vukovic Nikola <admin@nikolav.rs>
2024-04-30 13:48:07 +0200

    Tue Apr 30 13:48:07 CEDT 2024

--- composables/useStoryImage.ts
+++ composables/useStoryImage.ts
@@ -1,16 +1,19 @@
-import type { IDoc, IStorageFileInfo } from "@/types";
+import type { IDoc, IStorageFileInfo, OrNoValue } from "@/types";
 import { Q_postsImages, M_postsImagesDrop } from "@/graphql";
 
 export const useStoryImage = (SID?: any) => {
   const {
     graphql: { STORAGE_QUERY_POLL_INTERVAL },
     io: { IOEVENT_STORY_PHOTOS_CHANGE_prefix },
+    key: { POST_IMAGES_prefix },
   } = useAppConfig();
   const sid$ = ref();
   const enabled_ = computed(() => !!sid$.value);
   watchEffect(() => {
     sid$.value = toValue(SID);
   });
+  const { upload, publicUrl } = useApiStorage();
+  const { tags } = useDocs();
   const { result, refetch, load, loading, error } = useLazyQuery<{
     postsImages: IDoc<IStorageFileInfo>[];
   }>(
@@ -24,23 +27,53 @@ export const useStoryImage = (SID?: any) => {
     }
   );
   const image = computed(() => first(get(result.value, "postsImages")));
+  const src = computed(() => publicUrl(get(image.value, "data.file_id")));
   onceMountedOn(true, load);
   const reload = async () => await refetch();
 
   const { mutate: mutatePostsImagesDrop } = useMutation(M_postsImagesDrop);
   const dropImages = async () =>
-    enabled_.value && (await mutatePostsImagesDrop({ id: sid$.value }));
+    enabled_.value
+      ? await mutatePostsImagesDrop({ id: sid$.value })
+      : undefined;
+
+  // set new story image
+  const update = async (file: OrNoValue<File> = null) => {
+    if (!enabled_.value) return;
+    if (!file) return;
+
+    if (image.value) {
+      await dropImages();
+    }
+    const newImageId = Number(
+      get(
+        await upload({
+          image: { file },
+        }),
+        "image.id"
+      )
+    );
+    if (newImageId) {
+      await tags(newImageId, {
+        [`${POST_IMAGES_prefix}${sid$.value}:${newImageId}`]: true,
+      });
+    }
+  };
 
   const ioEvent = computed(() =>
     enabled_.value ? `${IOEVENT_STORY_PHOTOS_CHANGE_prefix}${sid$.value}` : ""
   );
   watchEffect(() => useIOEvent(ioEvent.value, reload));
+
   return {
     sid$,
     image,
+    src,
+    update,
     dropImages,
     reload,
     error,
     loading,
+    IO: ioEvent,
   };
 };
--- pages/demo.vue
+++ pages/demo.vue
@@ -5,12 +5,27 @@ definePageMeta({
   layout: "blank",
 });
 
+const { image, dropImages, reload, update, src: src$ } = useStoryImage(71);
+const image$ = ref();
+const upl = async () => await update(image$.value);
 // #eos
 </script>
 <template>
   <section class="page--demo.index">
-    <!-- <hr />
-    <Dump :data="{ urls$, files }" /> -->
+    <VBtn @click="upl">upl</VBtn>
+    <VBtn @click="dropImages">rm</VBtn>
+    <VBtn @click="reload">reload</VBtn>
+    <VFileInput
+      @update:modelValue="
+        ([file]) => {
+          image$ = file;
+        }
+      "
+      label="file"
+    />
+    <hr />
+    <Dump :data="{ image }" />
+    <VImg class="bg-grey" width="312" height="312" cover :src="src$" />
   </section>
 </template>
 <style lang="scss" scoped>
