c1f11ffc1a8bf5fe07d5a767013b2396c3b0a2b7
Vukovic Nikola <admin@nikolav.rs>
2024-05-22 10:18:17 +0200

    invoices

--- composables/useSavePdf.ts
+++ composables/useSavePdf.ts
@@ -1,14 +1,21 @@
 import fileSaver from "file-saver";
-import { URL_PDF_DL } from "@/config";
+import { URL_PDF_DL, URL_APP_PUBLIC, ENDPOINT_GRAPHQL } from "@/config";
 
 const { saveAs } = fileSaver;
 
+const query = `
+  query Q_pdfDownload($data: JsonData!) {
+    pdfDownload(data: $data)
+  }
+`;
 export const useSavePdf = () => {
+  const { watchProcessing } = useStoreAppProcessing();
   const procSavePdf = useProcessMonitor();
   const auth = useStoreApiAuth();
   const {
     pdf: { DEFAULT_DOWNLOAD_FILENAME },
   } = useAppConfig();
+  watchProcessing(() => procSavePdf.processing.value);
   // render pdf endpoint
   //  load data in --headless chrome;
   //   save bytes client side
@@ -19,26 +26,37 @@ export const useSavePdf = () => {
   }) => {
     try {
       procSavePdf.begin();
-      const pdf: Blob = await $fetch(URL_PDF_DL, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${auth.token$}`,
-        },
-        body: data,
-        responseType: "arrayBuffer",
-      });
-      saveAs(new Blob([pdf]), filename);
+      const d = get(
+        await $fetch(ENDPOINT_GRAPHQL, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${auth.token$}`,
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: {
+            query: query,
+            variables: {
+              data,
+            },
+          },
+        }),
+        "data.pdfDownload"
+      );
+      const bs = atob(d ? String(d) : "");
+      const len = bs.length;
+      const bytes = new Uint8Array(len);
+      for (let i = 0; i < len; i++) {
+        bytes[i] = bs.charCodeAt(i);
+      }
+      saveAs(new Blob([bytes.buffer]), filename);
     } catch (error) {
       procSavePdf.setError(error);
     } finally {
       procSavePdf.done();
     }
     if (!procSavePdf.error.value) procSavePdf.successful();
-    return { error: procSavePdf.error.value };
   };
-  const { watchProcessing } = useStoreAppProcessing();
-  watchProcessing(() => procSavePdf.processing.value);
-
   return {
     savePdf,
   };
--- pages/demo.vue
+++ pages/demo.vue
@@ -1,19 +1,72 @@
 <script setup lang="ts">
 import { Dump } from "@/components/dev";
+import { saveAs } from "file-saver";
+import { ENDPOINT_GRAPHQL } from "@/config";
+import { template } from "lodash";
 
 definePageMeta({
   layout: "blank",
 });
 
 const { savePdf } = useSavePdf();
+
 const dl = async () => {
-  console.log(
-    await savePdf({
-      filename: "file.pdf",
-      data: { name: "time", value: Date.now() },
-    })
-  );
+  await savePdf({
+    filename: `out--${idGen()}.pdf`,
+    data: {
+      template: "order-items",
+      uid: 1,
+      oid: 1,
+    },
+  });
 };
+
+// const auth = useStoreApiAuth();
+// const dl = async () => {
+//   // await savePdf({
+//   //   filename: "out.pdf",
+//   //   data: {
+//   //     template: "order-items",
+//   //     oid: 1,
+//   //     uid: 1,
+//   //   },
+//   // });
+//   const query = `
+//     query Q_pdfDownload($data: JsonData!) {
+//       pdfDownload(data: $data)
+//     }
+//   `;
+//   const d = get(
+//     await $fetch(ENDPOINT_GRAPHQL, {
+//       method: "POST",
+//       headers: {
+//         Authorization: `Bearer ${auth.token$}`,
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       body: {
+//         query: query,
+//         variables: {
+//           data: {
+//             template: "order-items",
+//             oid: 1,
+//             uid: 1,
+//           },
+//         },
+//       },
+//     }),
+//     "data.pdfDownload"
+//   );
+
+//   const bs = atob(d ? String(d) : "");
+//   const len = bs.length;
+//   const bytes = new Uint8Array(len);
+//   for (let i = 0; i < len; i++) {
+//     bytes[i] = bs.charCodeAt(i);
+//   }
+//   // console.log(new Blob([bytes.buffer]))
+//   saveAs(new Blob([bytes.buffer]), "file.pdf");
+// };
 // #eos
 </script>
 <template>
